44fc77e365007403fc4dc378964a68f0a375ab43
Pass Automated Testing Suite <Pass-Automated-Testing-Suite@zx2c4.com>
2023-05-03 14:55:01 +0200

    Añadido sopa.c

---
+++ source/sopa.c
@@ -0,0 +1,84 @@
+void escribir_mensaje_bienvenida() {
+}
+
+palabra_t *leer_palabras(char *nombre_fichero, int *n_pal) {
+}
+
+palabra_t *nueva_palabra(char *s) {
+}
+
+void insertar_palabra_en_lista(palabra_t *primera, palabra_t *nueva) {
+}
+
+void insertar_despues(palabra_t *a, palabra_t *b) {
+}
+
+void mostrar_palabras (palabra_t *p, int n) {
+}
+
+int pedir_tamano_sopa(int min, int max) {
+}
+
+sopa_t *generar_sopa(int dim, palabra_t *p) {
+}
+
+void insertar_palabras(sopa_t *j) {
+}
+
+void insertar_palabra(sopa_t *j, palabra_t *p) {
+}
+
+void posicion_aleatoria(palabra_t *p, int dim) {
+}
+
+int ran_int(int min, int max) {
+}
+
+bool comprobar_posicion_valida(sopa_t *j, palabra_t *p) {
+}
+
+int buscar_casilla(int dim, palabra_t *p, int i) {
+}
+
+void escribir_palabra(sopa_t *j, palabra_t *p) {
+}
+
+void rellenar_espacios_vacios(sopa_t *j) {
+}
+
+bool jugada_usuario(sopa_t *j) {
+}
+
+palabra_t *encontrar_palabra_lista(palabra_t p, char *s) {
+}
+
+void pedir_coordenadas(palabra_t *p) {
+}
+
+bool comprobar_palabras_iguales(palabra_t *a, palabra_t *b) {
+}
+
+void marcar_acierto(sopa_t *j, palabra_t *p) {
+}
+
+void marcar_letras_acertadas(sopa_t *j, palabra_t *p) {
+}
+
+void eliminar_palabra_lista(sopa_t *j, palabra_t *p) {
+}
+
+void mostrar_solucion(sopa_t *j) {
+}
+
+void liberar_palabras(palabra_t *p) {
+}
+
+void mostrar_sopa(sopa_t *j) {
+}
+
+void felicitar_jugador() {
+}
+
+void liberar_memoria(sopa_t *j) {
+}
+
